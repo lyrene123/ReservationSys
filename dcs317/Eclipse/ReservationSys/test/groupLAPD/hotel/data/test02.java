486917cae4ee2d40c9659298895308dc5b1b5a88
DanielCavalcanti <danielhbc@gmail.com>
2016-09-06 11:32:02 -0400

    test update

---
+++ dcs317/Eclipse/ReservationSys/test/groupLAPD/hotel/data/test02.java
@@ -0,0 +1,9 @@
+package groupLAPD.hotel.data;
+
+public class test02 {
+
+	public test02() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ dcs317/Eclipse/ReservationSys/test/groupLAPD/hotel/ui/test04.java
@@ -0,0 +1,9 @@
+package groupLAPD.hotel.ui;
+
+public class test04 {
+
+	public test04() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
