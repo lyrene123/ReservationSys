1aff203b9e4b4be35ee33748695f88373bab31ff
DanielCavalcanti <danielhbc@gmail.com>
2016-09-06 11:27:06 -0400

    Teste

---
+++ dcs317/Eclipse/ReservationSys/src/dw317/hotel/Teste.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package dw317.hotel;
+
+/**
+ * @author Daniel Bezerra
+ *
+ */
+public class Teste {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
