e78486717e6dc812f8676b89bd184c62f57fb7b6
Lyrene Labor <Lyrene Labor@10.230.37.46>
2016-09-07 23:55:54 -0400

    AbstractCreditCard class update

--- dcs317/Eclipse/ReservationSys/src/dw317/lib/creditcard/AbstractCreditCard.java
+++ dcs317/Eclipse/ReservationSys/src/dw317/lib/creditcard/AbstractCreditCard.java
@@ -14,12 +14,60 @@ public abstract class AbstractCreditCard implements CreditCard{
 	private final String number;
 	
 	public AbstractCreditCard(CardType cardType, String number) throws IllegalArgumentException{
-		this.cardType = cardType;
-		this.number = number;
 		
 		if (number == null || number == "" || cardType == null){
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("The input values of type CardType and "
+					+ "String for AbstractCreditCard method are invalid");
 		} 
+		
+		this.cardType = cardType;
+		this.number = number;	
 	}
+	
+	@Override
+	public String getNumber(){
+		return this.number;
+	} 
+	
+	@Override
+	public CardType getType(){
+		return this.cardType;
+	}
+	
+	@Override
+	public String toString(){
+		return this.cardType.toString() + "*" + this.number;
+	}
+	
+	@Override
+	public boolean equals(Object object){
+		if(object.getClass()==this.getClass()){
+			CreditCard otherCard = (CreditCard)object;
+			if(this.getNumber().equals(otherCard.getNumber()) &&
+					this.getType().equals(otherCard.getType())){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+		else{
+			return false;
+		}
+	}
+	
+	@Override
+	public int hashCode(){
+		final int prime = 37;
+		int result = 1;
+		result = prime*result+((number==null) ? 0 : number.hashCode());
+		return result;
+	}
+	
+	
+	
+	
+	
+	
 	
 }
