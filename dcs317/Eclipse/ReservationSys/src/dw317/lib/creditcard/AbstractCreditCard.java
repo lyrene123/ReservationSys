1f9bb6db662a3cca13a9d09dff666d596f30f7d9
Lyrene Labor <Lyrene Labor@10.230.37.46>
2016-09-08 17:40:34 -0400

    Luhn Algorithm done and tested Abstract class done so far

--- dcs317/Eclipse/ReservationSys/src/dw317/lib/creditcard/AbstractCreditCard.java
+++ dcs317/Eclipse/ReservationSys/src/dw317/lib/creditcard/AbstractCreditCard.java
@@ -16,12 +16,13 @@ public abstract class AbstractCreditCard implements CreditCard{
 	public AbstractCreditCard(CardType cardType, String number) throws IllegalArgumentException{
 		
 		if (number == null || number == "" || cardType == null){
-			throw new IllegalArgumentException("The input values of type CardType and "
-					+ "String for AbstractCreditCard method are invalid");
+			throw new IllegalArgumentException("The credit card type and number"
+					+ " must exist and should not be empty");
 		} 
 		
+		this.number = validateLuhnAlgorithm(number);
 		this.cardType = cardType;
-		this.number = number;	
+		
 	}
 	
 	@Override
@@ -56,18 +57,52 @@ public abstract class AbstractCreditCard implements CreditCard{
 		}
 	}
 	
-	@Override
+	/*@Override
 	public int hashCode(){
 		final int prime = 37;
 		int result = 1;
 		result = prime*result+((number==null) ? 0 : number.hashCode());
 		return result;
+	}*/
+	
+	private static String validateLuhnAlgorithm(String number)throws IllegalArgumentException {
+		
+		long num=0;
+		try {
+			num = Long.parseLong(number);
+		}
+		catch(NumberFormatException e){
+			throw new IllegalArgumentException("The credit card number must be a non-decimal "
+					+ "numeric value containing less than 18 digits with no spaces");
+		}
+		
+		int position = 0;
+		int digit = 0;
+		int sum = 0;
+		
+		while(num>0){
+			digit = (int)(num%10);
+			if((position%2)!=0){
+				digit = digit*2;
+			}
+			if(digit>9){
+				digit = digit - 9;
+			}
+			
+			sum = sum + digit;
+			
+			position++;
+			num = num/10;   
+		}
+		
+		if(sum%10==0){
+			return number;
+		}
+		else{
+			throw new IllegalArgumentException("The credit card number is not a valid number "
+					+ "for a credit card based on the Luhn Algorithm");
+		}
+		
 	}
-	
-	
-	
-	
-	
-	
 	
 }
