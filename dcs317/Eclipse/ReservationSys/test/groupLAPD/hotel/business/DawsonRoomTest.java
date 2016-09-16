c9e8c7c26f31f338d736898d73cbc68f712ae881
Lyrene Labor <Lyrene Labor@192.168.2.85>
2016-09-16 08:28:00 -0400

    test

---
+++ dcs317/Eclipse/ReservationSys/test/groupLAPD/hotel/business/DawsonRoomTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package groupLAPD.hotel.business;
+
+/**
+ * The DawsonRoomTest class is used to test the methods of the DawsonRoom class
+ * @author Lyrene Labor
+ * @version September 2016
+ */
+public class DawsonRoomTest {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
