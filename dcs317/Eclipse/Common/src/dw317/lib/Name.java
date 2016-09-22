eebcc00e941145bf741b88ba63a18ccfc9989740
alman <alman@10.230.28.205>
2016-09-22 12:11:20 -0400

    updated

--- dcs317/Eclipse/Common/src/dw317/lib/Name.java
+++ dcs317/Eclipse/Common/src/dw317/lib/Name.java
@@ -22,8 +22,8 @@ public class Name implements Serializable {
 	 * @param lastName
 	 */
 	public Name(String firstName, String lastName){
-		this.firstName = firstName;
-		this.lastName = lastName;
+		this.firstName = validateExist(firstName);
+		this.lastName = validateExist(lastName);
 	}
 
 	/**
@@ -47,15 +47,16 @@ public class Name implements Serializable {
  	 * @param firstName
 	 */
 	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+		this.firstName = validateExist(firstName);
 	}
 
 	/**
 	 * Sets value to LastName 
 	 * @param lastName
 	 */
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public void setLastName(String lastName)
+	{
+		this.lastName = validateExist(lastName);
 	}
 
 	/**
@@ -116,26 +117,55 @@ public class Name implements Serializable {
 	
 	/**
 	 * Validate name, checks if name is null, empty or invalid
-	 * @return True 
+	 * @return a boolean true if the name is Valid, false if not Valid
 	 */
 	private boolean isValide(){
 		
 		if(firstName == null || lastName == null){
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Invalid Input-- Must Provide first name and Last Name");
+		}
+		
+		String trimmedFName = firstName.trim();
+		String trimmedLName = lastName.trim();
+		
+		if(trimmedFName.isEmpty() || trimmedLName.isEmpty()){
+			throw new IllegalArgumentException("Invalid Input-- Must Provide first name and Last Name");
 		}
 		
 		for(int i = 0; i < firstName.length(); i++){
 			if(!Character.isAlphabetic(firstName.charAt(i)) && firstName.charAt(i) != '\'' 
 					&& firstName.charAt(i) != '-' && firstName.charAt(i) != ' '){
-				throw new IllegalArgumentException();
+				throw new IllegalArgumentException("Invalid Input-- First Name must only contain alphabets, \"'\", \"-\" and space");
 			}
 		}
 		for(int i = 0; i < lastName.length(); i++){
 			if(!Character.isAlphabetic(lastName.charAt(i)) && lastName.charAt(i) != '\'' 
 					&& lastName.charAt(i) != '-' && lastName.charAt(i) != ' '){
-				throw new IllegalArgumentException();
+				throw new IllegalArgumentException("Invalid Input-- Last Name must only contain alphabets, \"'\", \"-\" and space");
 			}
 		}
 		return true;
+	}
+	
+	
+	
+	/**
+	 * Checks if the String is a valid firstName or lastName
+	 * @param name first name or last name
+	 * @return returns the String if it is valid
+	 */
+	public String validateExist(String name){
+		
+		if(name == null){
+			throw new IllegalArgumentException("Invalid Input-- Must Provide name");
+		}
+		
+		String trimmedName = name.trim();
+		
+		if(trimmedName.isEmpty()){
+			throw new IllegalArgumentException("Invalid Input-- Must Provide first name and Last Name");
+		}
+		
+		return name;
 	}
 }
