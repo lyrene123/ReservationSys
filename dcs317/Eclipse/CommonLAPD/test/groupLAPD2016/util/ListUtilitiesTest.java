8f828a1bb73f8c1ba4f129fa9f3c486029ddc443
DanielCavalcanti <danielhbc@gmail.com>
2016-10-26 00:37:01 -0400

    Updating ListUtilitiesTest Class (Also Unfinished)

--- dcs317/Eclipse/CommonLAPD/test/groupLAPD2016/util/ListUtilitiesTest.java
+++ dcs317/Eclipse/CommonLAPD/test/groupLAPD2016/util/ListUtilitiesTest.java
@@ -2,27 +2,27 @@ package groupLAPD2016.util;
 
 import java.io.File;
 import java.io.IOException;
-
-import dw317.lib.Name;
+import java.util.Arrays;
+import dw317.lib.Email;
 
 public class ListUtilitiesTest {
 
 	public static void main(String[] args) {
 
-		Name[] nameList = new Name[4];
-		nameList[0] = new Name("Daniel", "Cavalcanti");
-		nameList[1] = new Name("Ali", "Dali");
-		nameList[2] = new Name("PengKim", "Sy");
-		nameList[3] = new Name("Lyrene", "Labor");
+		testSortArrays();
 
-		//printArray(nameList);
 		
+		Email[] emailList = new Email[4];
+		emailList[0] = new Email("danielhbc@gmail.com");
+		emailList[1] = new Email("laborlyrene@gmail.com");
+		emailList[2] = new Email("alidali@gmail.com");
+		emailList[3] = new Email("pengKim@gmail.com");
+
 		File unsortedDir = new File("/datafiles/unsorted");
-		System.out.println(unsortedDir.mkdirs());
-		System.out.println(unsortedDir.getPath());
+		unsortedDir.mkdirs();
 
 		try {
-			ListUtilities.saveListToTextFile(nameList, "/datafiles/unsorted/nameDB.txt");
+			ListUtilities.saveListToTextFile(emailList, "/datafiles/unsorted/nameDB.txt");
 		} catch (IOException e) {
 			System.out.println(e.getMessage() + "\n\nExiting the Application.");
 			System.exit(1);
@@ -30,13 +30,89 @@ public class ListUtilitiesTest {
 
 	}
 
-	private static void printArray(Object[] anArray) {
-		for (int i = 0; i < anArray.length; i++) {
+	public static void testSortArrays() {
+		Email[] emailList = new Email[4];
+		emailList[0] = new Email("danielhbc@gmail.com");
+		emailList[1] = new Email("laborlyrene@gmail.com");
+		emailList[2] = new Email("alidali@gmail.com");
+		emailList[3] = new Email("pengKim@gmail.com");
+
+		Integer[] values = new Integer[] { 9, -3, 5, 0, 1 };
+		Double[] numbers = new Double[] { 8.1, 0.0, -1.1, 3.2, 5.4 };
+		String[] names = new String[] { "PengKim", "Ali", "Daniel", "Lyrene" };
+		Boolean[] bol = new Boolean[] {true, false, true, false};
+		Integer[] values2 = new Integer[] {};
+
+		System.out.println("\nTesting the generic static sort method from ListUtilities Class");
+		System.out.println("-----------------------------------------------------------------");
+		ListUtilitiesTest.<Integer>
+		testSortArrays("Case 1: A valid Integer Array with full capacity", values, true);
+		ListUtilitiesTest.<String>
+		testSortArrays("Case 2: A valid String Array with full capacity", names, true);
+		ListUtilitiesTest.<Double>
+		testSortArrays("Case 3: A valid Double Array with full capacity", numbers, true);
+		ListUtilitiesTest.<Email>
+		testSortArrays("Case 4: A valid Double Array with full capacity", emailList, true);
+		ListUtilitiesTest.<Boolean>
+		testSortArrays("Case 5: A valid Boolean Array with full capacity", bol, true);
+		ListUtilitiesTest.<Integer>
+		testSortArrays("Case 6: Empty String", values2, false);
+	}
+
+	public static void testSortEmptyArray() {
+
+		Integer[] values = new Integer[4];
+		try {
+			ListUtilities.<Integer>sort(values);
+			printArrayObjects(values);
+		} catch (NullPointerException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+	public static void testSortOneElementArray() {
+		Integer[] values = new Integer[] { 42 };
+
+		ListUtilities.<Integer>sort(values);
+		printArrayObjects(values);
+
+	}
+
+	public static <T extends Comparable<T>> void testSortArrays(String testCase, T[] obj, boolean expectedValid) {
+
+		try {
+			System.out.println("   " + testCase);
+			System.out.print("\tArray Before Sorting: ");
+			System.out.println(Arrays.toString(obj));
+			ListUtilities.<T>sort(obj);
+			System.out.print("\tArray After Sorting:  ");
+			System.out.println(Arrays.toString(obj));
+
+			System.out.println("End of testeCase");
+
+		} catch (Exception e) {
+			System.out.print(
+					"\tUNEXPECTED EXCEPTION TYPE! " + e.getClass() + " " + e.getMessage() + " ==== FAILED TEST ====");
+
+		} finally {
+			System.out.println("____________________________________________________________");
+		}
+
+	}
+
+	private static void printArrayObjects(Object[] anArrayObject) {
+		for (int i = 0; i < anArrayObject.length; i++) {
 			if (i > 0) {
-				System.out.print("; ");
+				System.out.print(",");
 			}
-			System.out.print(anArray[i]);
+			System.out.print(anArrayObject[i]);
 		}
 	}
+	/*
+	 * private static void printArray(int[] anArray) { for (int i = 0; i <
+	 * anArray.length; i++) { if (i > 0) { System.out.print(","); }
+	 * System.out.print(anArray[i]); } }
+	 */
 
 }
