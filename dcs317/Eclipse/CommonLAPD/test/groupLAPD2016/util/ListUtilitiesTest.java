2d6171698ff3e478d39fdb6af535f4a7f78223f1
DanielCavalcanti <danielhbc@gmail.com>
2016-10-27 00:49:51 -0400

    ListUtilitiesTest updated (fixed some bugs - still unfinished)

--- dcs317/Eclipse/CommonLAPD/test/groupLAPD2016/util/ListUtilitiesTest.java
+++ dcs317/Eclipse/CommonLAPD/test/groupLAPD2016/util/ListUtilitiesTest.java
@@ -35,13 +35,15 @@ public class ListUtilitiesTest {
 		emailList[0] = new Email("danielhbc@gmail.com");
 		emailList[1] = new Email("laborlyrene@gmail.com");
 		emailList[2] = new Email("alidali@gmail.com");
-		emailList[3] = new Email("pengKim@gmail.com");
+		
+		
 
 		Integer[] values = new Integer[] { 9, -3, 5, 0, 1 };
 		Double[] numbers = new Double[] { 8.1, 0.0, -1.1, 3.2, 5.4 };
 		String[] names = new String[] { "PengKim", "Ali", "Daniel", "Lyrene" };
 		Boolean[] bol = new Boolean[] {true, false, true, false};
 		Integer[] values2 = new Integer[] {};
+		Integer[] values3 = new Integer[8];
 
 		System.out.println("\nTesting the generic static sort method from ListUtilities Class");
 		System.out.println("-----------------------------------------------------------------");
@@ -52,11 +54,14 @@ public class ListUtilitiesTest {
 		ListUtilitiesTest.<Double>
 		testSortArrays("Case 3: A valid Double Array with full capacity", numbers, true);
 		ListUtilitiesTest.<Email>
-		testSortArrays("Case 4: A valid Double Array with full capacity", emailList, true);
+		testSortArrays("Case 4: A Double Array not full to capacity", emailList, false);
 		ListUtilitiesTest.<Boolean>
 		testSortArrays("Case 5: A valid Boolean Array with full capacity", bol, true);
 		ListUtilitiesTest.<Integer>
-		testSortArrays("Case 6: Empty String", values2, false);
+		testSortArrays("Case 6: Empty Array", values2, false);
+		ListUtilitiesTest.<Integer>
+		testSortArrays("Case 7: Empty Array", values3, false);
+		
 	}
 
 	public static void testSortEmptyArray() {
@@ -88,12 +93,25 @@ public class ListUtilitiesTest {
 			ListUtilities.<T>sort(obj);
 			System.out.print("\tArray After Sorting:  ");
 			System.out.println(Arrays.toString(obj));
+			if(expectedValid)
+			{
+				System.out.println("\tExpected Result! TEST OK");
+			}else{
+				System.out.println("\n\tUnexpected Error==== FAILED TEST ====");
+			}
 
-			System.out.println("End of testeCase");
+			System.out.println("\tEnd of testeCase");
 
 		} catch (Exception e) {
 			System.out.print(
-					"\tUNEXPECTED EXCEPTION TYPE! " + e.getClass() + " " + e.getMessage() + " ==== FAILED TEST ====");
+					"\tUNEXPECTED EXCEPTION TYPE! " + e.getClass() + " " + e.getMessage());
+			System.out.println();
+			if(!expectedValid)
+			{
+				System.out.println("\tExpected Error! TEST OK");
+			}else{
+				System.out.println("\n\tUnexpected Error==== FAILED TEST ====");
+			}
 
 		} finally {
 			System.out.println("____________________________________________________________");
@@ -109,10 +127,5 @@ public class ListUtilitiesTest {
 			System.out.print(anArrayObject[i]);
 		}
 	}
-	/*
-	 * private static void printArray(int[] anArray) { for (int i = 0; i <
-	 * anArray.length; i++) { if (i > 0) { System.out.print(","); }
-	 * System.out.print(anArray[i]); } }
-	 */
-
+	
 }
