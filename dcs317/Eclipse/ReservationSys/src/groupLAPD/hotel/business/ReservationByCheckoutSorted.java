aa9f01a0be64be11acba82e980ccba566abce10c
KimHyonh <KimHyonh@KimHyonh-PC>
2016-10-28 10:41:01 -0300

    Added comment to these classes

--- dcs317/Eclipse/ReservationSys/src/groupLAPD/hotel/business/ReservationByCheckoutSorted.java
+++ dcs317/Eclipse/ReservationSys/src/groupLAPD/hotel/business/ReservationByCheckoutSorted.java
@@ -3,8 +3,24 @@ package groupLAPD.hotel.business;
 import java.util.Comparator;
 import dw317.hotel.business.interfaces.Reservation;
 
+/**
+ * ReservationByCheckoutSorted implements the Comparator<Reservation> interface to
+ * enable sorting of Reservations on checkout date. The Comparator
+ * interface defines the compare method.
+ * 
+ * @author Pengkim Sy
+ * @version 28/10/2016
+ */
 public class ReservationByCheckoutSorted implements Comparator<Reservation>{
 
+	/**
+	 * This method is to compare the first reservation with the second reservation
+	 * base on their check out date. If two reservations have the same checkout date,
+	 *  order them by the default order (i.e., compareTo).
+	 * 
+	 * @param the first Reservation
+	 * @param the second Reservation
+	 */
 	@Override
 	public int compare(Reservation r1, Reservation r2) {
 		
