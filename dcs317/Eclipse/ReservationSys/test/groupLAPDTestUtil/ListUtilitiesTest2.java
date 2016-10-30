27323172f444e480fdd08f72632159c245fe99ed
DanielCavalcanti <danielhbc@gmail.com>
2016-10-30 17:25:48 -0400

    ListUtilitiesTest2 updated

--- dcs317/Eclipse/ReservationSys/test/groupLAPDTestUtil/ListUtilitiesTest2.java
+++ dcs317/Eclipse/ReservationSys/test/groupLAPDTestUtil/ListUtilitiesTest2.java
@@ -13,46 +13,16 @@ import dw317.lib.creditcard.CreditCard;
 import dw317.lib.creditcard.Visa;
 import groupLAPD.hotel.business.*;
 import groupLAPD2016.util.ListUtilities;
-import groupLAPD2016.util.ListUtilitiesTest;
 
 
 public class ListUtilitiesTest2 {
 	
 		public static void main(String[] args) {
 
-			//testSort();
+			testSort();
 			testMerge();
 			testSort2param();
 
-			Email[] emailList = new Email[3];
-			emailList[0] = new Email("danielhbc@gmail.com");
-			emailList[1] = new Email("laborlyrene@gmail.com");
-			emailList[2] = new Email("alidali@gmail.com");
-			
-				
-			
-			/*String[] nameList = new String[] { "PengKim", "Ali", "Daniel", "Lyrene" };
-			File unsortedDir = new File("/datafiles/unsorted");
-			unsortedDir.mkdirs();
-			*/
-			/*StringBuilder path = new StringBuilder("datafiles/duplicates/");
-			path.append("duplicateTest.txt");
-			System.out.println(path);
-			
-			try {
-				ListUtilities.saveListToTextFile(emailList,
-						path.toString(), true);
-			} catch (IOException e) {
-				System.out.println(e.getMessage() + "\n\nExiting the Application.");
-				System.exit(1);
-			}
-			*/
-			/*
-			 * try { ListUtilities.saveListToTextFile (nameList,
-			 * "D:/LAPD2016/dcs317/Eclipse/ReservationSys/datafiles/duplicates.txt")
-			 * ; } catch (IOException e) { System.out.println(e.getMessage() +
-			 * "\n\nExiting the Application."); System.exit(1); }
-			 */
 		}
 
 		public static void testSort() {
@@ -70,45 +40,49 @@ public class ListUtilitiesTest2 {
 
 			System.out.println("\nTesting the generic static sort method from ListUtilities Class");
 			System.out.println("-----------------------------------------------------------------");
-			ListUtilitiesTest.<Integer>
+			ListUtilitiesTest2.<Integer>
 			testSort("Case 1: A valid Integer Array with full capacity", values, true);
-			ListUtilitiesTest.<String>
+			ListUtilitiesTest2.<String>
 			testSort("Case 2: A valid String Array with full capacity", names, true);
-			ListUtilitiesTest.<Double>
+			ListUtilitiesTest2.<Double>
 			testSort("Case 3: A valid Double Array with full capacity", numbers, true);
-			ListUtilitiesTest.<Email>
+			ListUtilitiesTest2.<Email>
 			testSort("Case 4: A Double Array not full to capacity", emailList, false);
-			ListUtilitiesTest.<Boolean>
+			ListUtilitiesTest2.<Boolean>
 			testSort("Case 5: A valid Boolean Array with full capacity", bol, true);
-			ListUtilitiesTest.<Integer>
+			ListUtilitiesTest2.<Integer>
 			testSort("Case 6: Empty Array", values2, false);
-			ListUtilitiesTest.<Integer>
+			ListUtilitiesTest2.<Integer>
 			testSort("Case 7: Empty Array", values3, false);
 
 		}
 		public static void testMerge(){
 			
-			String[] nameList2 = new String[] { "Ali", "Carlos", "Frank", "Lyrene" };
-			String[] nameList1 = new String[] { "Ali", "Frank","Lyrene"};
+			String[] nameList1 = new String[] { "Ali", "Carlos", "Frank", "Lyrene" };
+			String[] nameList2 = new String[] { "Ali", "Frank","Lyrene","Marcy"};
 			String[] nameList3 = new String[] { "j@doe.com*John*Doe*visa*4556737586899855"};
 			String[] nameList4 = new String[] { "j@doe.com*John*Doe**"};
-			
-			Visa visacard = new Visa("4774341863020040");
-			Visa visacard2 = new Visa("4929593056772795");
-			Amex visacard3 = new Amex("344322624384908");
+			String[] nameList5 = new String[] { "Marcy", "Sarah", "Nancy", "Maria" };
+			String[] nameList6 = new String[4];
+			String[] nameList7 = new String[] { "Duardo", "Durin", "Marcio", "Marcos" };
+			String[] nameList8 = new String[] {};
+			String[] nameList9 = new String[] {};
+			String[] nameList10 = new String[] {};
 			
 			DawsonCustomer daniel = new DawsonCustomer("Daniel", "Bezerra"
 					,"danielhbc@gmail.com");
+			Visa visacard = new Visa("4774341863020040");
 			Optional<CreditCard> card = Optional.ofNullable(visacard);
 			daniel.setCreditCard(card);
 			
-			DawsonCustomer pengkim = new DawsonCustomer("Daniel", "Bezerra"
-					,"danielhbc@gmail.com");
+			DawsonCustomer pengkim = new DawsonCustomer("PengKim", "Sy"
+					,"pengkim@gmail.com");
 			Optional<CreditCard> card2 = Optional.ofNullable(visacard);
 			pengkim.setCreditCard(card2);
 			
 			DawsonCustomer jaya = new DawsonCustomer("Jaya", "Naka"
 					,"jaya@gmail.com");
+			Amex visacard3 = new Amex("344322624384908");
 			Optional<CreditCard> card3 = Optional.ofNullable(visacard3);
 			jaya.setCreditCard(card3);
 			
@@ -117,12 +91,24 @@ public class ListUtilitiesTest2 {
 			customerlist[1] = pengkim;
 			Customer[] customerlist2 = new DawsonCustomer[3]; 
 			customerlist2[0] = daniel;
-			customerlist2[1] = pengkim;
-			customerlist2[2] = jaya;
+			customerlist2[1] = jaya;
+			customerlist2[2] = pengkim;
 					
-				
-			ListUtilitiesTest.<String>
-			testMerge("Case 1: Merging", nameList2, nameList1, "duplicateNames.txt", true);
+			System.out.println("\nTesting the generic static merge method from ListUtilities Class");
+			System.out.println("-----------------------------------------------------------------");
+			ListUtilitiesTest2.<Customer>
+			testMerge("Case 1: Merging two comparable objects", customerlist, customerlist2, "duplicateNames.txt", true);
+			ListUtilitiesTest2.<String>
+			testMerge("Case 2: Merging", nameList1, nameList2, "duplicateNames.txt", true);
+			ListUtilitiesTest2.<String>
+			testMerge("Case 3: Merging", nameList3, nameList4, "duplicateNames.txt", true);
+			ListUtilitiesTest2.<String>
+			testMerge("Case 4: An attempt to merge one array with nulls only ", nameList5, nameList6, "duplicateNames.txt", false);
+			ListUtilitiesTest2.<String>
+			testMerge("Case 5: An attempt to merge one empty array", nameList7, nameList8, "duplicateNames.txt", false);
+			ListUtilitiesTest2.<String>
+			testMerge("Case 6: An attempt to merge two empty arrays", nameList9, nameList10, "duplicateNames.txt", false);
+			
 			
 		}
 
@@ -135,8 +121,14 @@ public class ListUtilitiesTest2 {
 				System.out.println(Arrays.toString(list1));
 				System.out.print("\tArray List2: ");
 				System.out.println(Arrays.toString(list2));
-				ListUtilities.<T>merge(list1, list2, duplicateFileName);
-
+				
+				@SuppressWarnings("rawtypes")
+				Comparable[] mergedList = new Comparable[0];
+				
+				mergedList = ListUtilities.<T>merge(list1, list2, duplicateFileName);
+				
+				System.out.print("\tArray mergedList: ");
+				System.out.println(Arrays.toString(mergedList));
 				if (expectedValid) {
 					System.out.println("\tExpected Result! TEST OK");
 				} else {
@@ -192,15 +184,6 @@ public class ListUtilitiesTest2 {
 
 		}
 
-		private static void printArrayObjects(Object[] anArrayObject) {
-			for (int i = 0; i < anArrayObject.length; i++) {
-				if (i > 0) {
-					System.out.print(",");
-				}
-				System.out.print(anArrayObject[i]);
-			}
-		}
-		
 		private static void testSort2param(){
 			
 			testSortByCustomer();
