8cc8949b1281813bf6f4efa29a17592bfe5f7bc3
1541901 <1541901@2F18-02.ad.dawsoncollege.qc.ca>
2016-11-10 11:55:37 -0500

    ReservationListDB

--- dcs317/Eclipse/ReservationSys/src/groupLAPD/hotel/data/ReservationListDB.java
+++ dcs317/Eclipse/ReservationSys/src/groupLAPD/hotel/data/ReservationListDB.java
@@ -17,6 +17,7 @@ import dw317.hotel.data.NonExistingReservationException;
 import dw317.hotel.data.interfaces.ListPersistenceObject;
 import dw317.hotel.data.interfaces.ReservationDAO;
 import groupLAPD.hotel.business.DawsonHotelFactory;
+import groupLAPD.hotel.business.DawsonReservation;
 
 /**
  * 
@@ -32,6 +33,7 @@ public class ReservationListDB implements ReservationDAO {
 	private final HotelFactory factory; 
 	
 	/**
+	 * 
 	 * 
 	 * @param listPersistenceObject
 	 * @author Lyrene Labor
@@ -80,9 +82,70 @@ public class ReservationListDB implements ReservationDAO {
 	@Override
 	public void add(Reservation reserv) 
 			throws DuplicateReservationException {
-		
-
+		for(int i = 0; i<this.database.size(); i++){
+			if(this.database.get(i).overlap(reserv)){
+				throw new DuplicateReservationException();
+			}
+		}
+		//make a copy of reserv
+		Reservation copyReserv = this.factory.getReservationInstance
+				(reserv.getCustomer(), 
+				reserv.getRoom(), reserv.getCheckInDate().getYear(), 
+				reserv.getCheckInDate().getMonthValue(), 
+				reserv.getCheckInDate().getDayOfMonth(), 
+				reserv.getCheckOutDate().getYear(), 
+				reserv.getCheckOutDate().getMonthValue(),
+				reserv.getCheckOutDate().getDayOfMonth());
+		insertToOrderedList(copyReserv, this.database);
 	}
+	
+	/**
+	 * 
+	 * @param reserv
+	 * @param database
+	 */
+	private static void insertToOrderedList(Reservation reserv, 
+											List<Reservation> database ){
+		boolean isAdded = false;
+		
+		int left = 0;
+		int right = database.size() - 1;
+		int middle = (left + right)/2;
+		while(right>=left){
+			if(database.get(middle).compareTo(reserv) < 0){
+				left = middle + 1;
+			}
+			else if(database.get(middle).compareTo(reserv)>0){
+				right = middle-1;
+			}
+			else{
+				database.add(middle, reserv);
+				isAdded = true;
+			}
+		}
+		
+		if(isAdded == false){
+			database.add(reserv);
+		}
+		
+		/**for(int i = 0; i< database.size(); i++){
+			//if the element from database is smaller than reserv, 
+			//skip to the next element
+			if(database.get(i).compareTo(reserv) < 0){
+				continue;
+			}
+			else{
+				//otherwise add reserv to the current position of the loop
+				database.add(reserv);
+				isAdded = true;
+			}
+		}
+		//if reserv hasn't been added, add at the end of the List
+		if(isAdded == false){
+			database.add(reserv);		
+		}*/
+	}
+	
 
 	/**
 	 * 
