c10079cbc89024e753b583b0e775d562dc1607e4
Lyrene Labor <Lyrene Labor@192.168.2.85>
2016-11-09 22:39:59 -0500

    Created ReservationListDB class and test class

---
+++ dcs317/Eclipse/ReservationSys/src/groupLAPD/hotel/data/ReservationListDB.java
@@ -0,0 +1,120 @@
+/**
+ * 
+ */
+package groupLAPD.hotel.data;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+
+import dw317.hotel.business.RoomType;
+import dw317.hotel.business.interfaces.Customer;
+import dw317.hotel.business.interfaces.HotelFactory;
+import dw317.hotel.business.interfaces.Reservation;
+import dw317.hotel.business.interfaces.Room;
+import dw317.hotel.data.DuplicateReservationException;
+import dw317.hotel.data.NonExistingReservationException;
+import dw317.hotel.data.interfaces.ListPersistenceObject;
+import dw317.hotel.data.interfaces.ReservationDAO;
+
+/**
+ * 
+ * 
+ * @author Lyrene Labor, Daniel Cavalcanti
+ *
+ */
+public class ReservationListDB implements ReservationDAO {
+	
+	private List<Reservation> database;  
+	private List<Room> allRooms;  
+	private final ListPersistenceObject listPersistenceObject;  
+	private final HotelFactory factory; 
+		
+	public ReservationListDB(List<Reservation> database,List<Room> allRooms,
+		ListPersistenceObject listPersistenceObject,HotelFactory factory ){
+		
+		this.listPersistenceObject = listPersistenceObject;
+		this.factory = factory;
+		this.database = database;
+		this.allRooms = this.listPersistenceObject.getRoomDatabase();
+		
+	}
+	
+	/**
+	 * 
+	 */
+	@Override
+	public void add(Reservation reserv) 
+			throws DuplicateReservationException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public void disconnect() throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public List<Reservation> getReservations(Customer cust) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public void cancel(Reservation reserv) 
+			throws NonExistingReservationException {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public List<Room> getReservedRooms(LocalDate checkin,
+			LocalDate checkout) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public List<Room> getFreeRooms(LocalDate checkin,
+			LocalDate checkout) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public List<Room> getFreeRooms(LocalDate checkin,
+			LocalDate checkout, RoomType roomType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public void clearAllPast() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ dcs317/Eclipse/ReservationSys/test/groupLAPD/hotel/data/ReservationListDBTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package groupLAPD.hotel.data;
+
+/**
+ * 
+ * 
+ * @author Lyrene Labor, Daniel Cavalcanti
+ *
+ */
+public class ReservationListDBTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
