eba447e498c9476c20158b546c6e2bb17c0c9f17
KimHyonh <KimHyonh@192.168.93.1>
2016-11-17 12:19:03 -0500

    allocationpolicy

--- dcs317/Eclipse/ReservationSys/src/dw317/hotel/business/interfaces/AllocationPolicy.java
+++ dcs317/Eclipse/ReservationSys/src/dw317/hotel/business/interfaces/AllocationPolicy.java
@@ -1,5 +1,17 @@
 package dw317.hotel.business.interfaces;
 
-public interface AllocationPolicy {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Optional;
 
+import dw317.hotel.business.RoomType;
+
+public interface AllocationPolicy extends Serializable {
+
+	/**
+	 * Returns the availlable room.
+	 * 
+	 * @return The visit of the next client booking the room.
+	 */
+	Optional<Room> getAvailableRoom(LocalDate checkin, LocalDate checkout, RoomType roomType);
 }
