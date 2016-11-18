1c62ad02768af3192dcecb40ab5032239cf82930
alman <alman@10.230.28.205>
2016-11-18 14:00:28 -0500

    final version we are done with JAVAA!!!

--- dcs317/Eclipse/ReservationSys/src/groupLAPD/hotel/data/ObjectSerializedList.java
+++ dcs317/Eclipse/ReservationSys/src/groupLAPD/hotel/data/ObjectSerializedList.java
@@ -3,7 +3,6 @@ package groupLAPD.hotel.data;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-
 import org.w3c.dom.ls.LSInput;
 
 import dw317.hotel.business.interfaces.Customer;
@@ -12,22 +11,37 @@ import dw317.hotel.business.interfaces.Room;
 import dw317.hotel.data.interfaces.ListPersistenceObject;
 import groupLAPD2016.util.Utilities;
 
+
+/**
+ * This class deserialzes the serialized files that have been given as input 
+ * from the constructor.
+ * @author Ali Dali
+ *
+ */
 public class ObjectSerializedList implements ListPersistenceObject{
-
-
 
 	private String roomFile;
 	private String customerFile;
 	private String reservationFile;
 
+	/**
+	 * 3 parameters constructor takes serialized files and assigns
+	 * them to the attributes
+	 * @param roomFile : serialized room File
+	 * @param customerFile : serialized File 
+	 * @param reservFile : serialized File
+	 */
 	public ObjectSerializedList(String roomFile, String customerFile, String reservFile){
 		this.roomFile = roomFile;
 		this.customerFile = customerFile;
 		this.reservationFile = reservFile;
 	}
 
-
-
+	/**
+	 * this method deserializes the serialized room file and assigns
+	 * the re-constructed object into a List
+	 * @return roomDatabase
+	 */
 	@Override
 	public List<Room> getRoomDatabase() {
 		
@@ -36,11 +50,15 @@ public class ObjectSerializedList implements ListPersistenceObject{
 			roomDatabase = (List<Room>) Utilities.deserializeObject(roomFile);
 		}catch(IOException | ClassNotFoundException e){
 			System.out.println(e.getMessage());
-		}
-		
+		}		
 		return roomDatabase;
 	}
 
+	/**
+	 * this method deserializes the serialized customer file and assigns
+	 * the re-constructed object into a List
+	 * @return customerDatabase
+	 */
 	@Override
 	public List<Customer> getCustomerDatabase() {
 
@@ -50,11 +68,15 @@ public class ObjectSerializedList implements ListPersistenceObject{
 			customerDatabase = (List<Customer>) Utilities.deserializeObject(customerFile);
 		}catch(IOException | ClassNotFoundException e){
 			System.out.println(e.getMessage());
-		}
-		
+		}		
 		return customerDatabase;
 	}
 
+	/**
+	 * this method deserializes the serialized reservation file and assigns
+	 * the re-constructed object into a List
+	 * @return reservDatabase
+	 */
 	@Override
 	public List<Reservation> getReservationDatabase() {
 
@@ -64,23 +86,27 @@ public class ObjectSerializedList implements ListPersistenceObject{
 		}catch(IOException | ClassNotFoundException e){
 			System.out.println(e.getMessage());
 		}
-		
 		return reservDatabase;
 	}
 
+	
+	/**
+	 * this method takes as input a customers list and serializes
+	 * it in the file that has been passed in as input in the constructor
+	 * @param List<Customer> : customer file we want to serialize
+	 */
 	@Override
 	public void saveCustomerDatabase(List<Customer> custs) throws IOException {
-
 		Utilities.serializeObject(custs, customerFile);
 	}
 
+	/**
+	 * this method takes as input a  Reservation list and serializes
+	 * it in the file that has been passed in as input in the constructor
+	 * @param List<Reservation> : Reservation file we want to serialize
+	 */
 	@Override
 	public void saveReservationDatabase(List<Reservation> reservs) throws IOException {
-		
 		Utilities.serializeObject(reservs, reservationFile);
 	}
-
-
-
-
 }
--- dcs317/Eclipse/ReservationSys/src/groupLAPD/hotel/data/SerializedFileLoaderApp.java
+++ dcs317/Eclipse/ReservationSys/src/groupLAPD/hotel/data/SerializedFileLoaderApp.java
@@ -2,22 +2,35 @@ package groupLAPD.hotel.data;
 
 import java.io.File;
 import java.io.IOException;
-
 import groupLAPD2016.util.Utilities;
 
+
+/**
+ * This App class creates the files that we want to store the serialized 
+ * data in.
+ * @author Ali Dali
+ *
+ */
 public class SerializedFileLoaderApp {
 
 	public static void main(String[] args){
+		
+		//path to the files 
 		String path = "datafiles" + File.separator + "database" 
 				+ File.separator;
 
+		//Creating an instance of SequentialTextFileList
+		//to be able to get the rooms, customers and reservations List
 		SequentialTextFileList loaded = new 
-				SequentialTextFileList(path+"rooms.txt", path+"customers.txt", path+"reservations.txt");
+				SequentialTextFileList(path+"rooms.txt", path+"customers.txt", 
+						path+"reservations.txt");
 
+		//creating the files
 		File roomFile = new File(path+"rooms.ser");
 		File customerFile = new File(path+"customers.ser");
 		File reservationFile = new File(path+"reservations.ser");
 
+		//creating the files
 		try{
 			roomFile.createNewFile();
 			customerFile.createNewFile();
@@ -26,17 +39,21 @@ public class SerializedFileLoaderApp {
 			System.out.println(e.getMessage());		
 		}
 		
+		//calling helper method
 		serializeObj(loaded, roomFile, customerFile, reservationFile);
 
 	}
-
-	public static void serializeObj(SequentialTextFileList loaded,File roomFile, File custFile, File resFile){
+	// helper method that serializes the files using Utilities class
+	private static void serializeObj(SequentialTextFileList loaded,
+			File roomFile, File custFile, File resFile){
+		//try to serialize 
 		try{
 			Utilities.serializeObject(loaded.getRoomDatabase(), roomFile.toString());
 			Utilities.serializeObject(loaded.getCustomerDatabase(), custFile.toString());
 			Utilities.serializeObject(loaded.getReservationDatabase(), resFile.toString());
+			//catch the exception
 		}catch(IOException io){
 			System.out.println(io.getMessage());
 		}
-	}
-}
+	}//end of serializeObj method
+}//end of class
