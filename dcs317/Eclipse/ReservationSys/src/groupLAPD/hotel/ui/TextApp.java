7e309d96ef9d1d830ec740de975b246b4269e2e0
1541901 <1541901@2F16-15.ad.dawsoncollege.qc.ca>
2016-12-09 16:01:24 -0500

    added some comments

--- dcs317/Eclipse/ReservationSys/src/groupLAPD/hotel/ui/TextApp.java
+++ dcs317/Eclipse/ReservationSys/src/groupLAPD/hotel/ui/TextApp.java
@@ -5,6 +5,14 @@ import dw317.hotel.data.interfaces.*;
 import groupLAPD.hotel.business.*;
 import groupLAPD.hotel.data.*;
 
+/**
+ * TextApp class is the application of the whole Dawson
+ * Reservation System project which implements a model, a view
+ * and a controller in order to display all required information
+ * to a user and to respond to any type of queries of the user
+ * @author provided by the teacher
+ *
+ */
 public class TextApp {
 	public static void main(String[] args) {
 		HotelFactory factory = 
