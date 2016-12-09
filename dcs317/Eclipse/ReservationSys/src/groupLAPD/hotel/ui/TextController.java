f4fa288bb8a9b14165436b592f5e844dad087a68
KimHyonh <KimHyonh@KimHyonh-PC>
2016-12-09 15:45:49 -0500

    TextController.java edited

--- dcs317/Eclipse/ReservationSys/src/groupLAPD/hotel/ui/TextController.java
+++ dcs317/Eclipse/ReservationSys/src/groupLAPD/hotel/ui/TextController.java
@@ -88,7 +88,6 @@ public class TextController {
 		} catch (DuplicateCustomerException e) {
 			System.out.println(e.getMessage());
 		} catch (IllegalArgumentException e){
-			System.out.println(e.getMessage());	
 		}
 		
 	}
