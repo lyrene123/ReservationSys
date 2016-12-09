91df59726c5ef4e389c545ab193be00b2b27ae7d
1541901 <1541901@2F18-06.ad.dawsoncollege.qc.ca>
2016-12-09 12:56:46 -0500

    text view

--- dcs317/Eclipse/ReservationSys/src/groupLAPD/hotel/ui/TextView.java
+++ dcs317/Eclipse/ReservationSys/src/groupLAPD/hotel/ui/TextView.java
@@ -1,5 +1,70 @@
 package groupLAPD.hotel.ui;
 
-public class TextView {
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Optional;
+
+import dw317.hotel.business.interfaces.Customer;
+import dw317.hotel.business.interfaces.Reservation;
+import dw317.lib.Email;
+import dw317.lib.Name;
+import dw317.lib.creditcard.AbstractCreditCard;
+import dw317.lib.creditcard.CreditCard;
+
+public class TextView implements Observer {
+
+	public TextView(Observable model) {
+		model.addObserver(this); //register
+	}
+	
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		
+		//reservation
+		if(arg instanceof Optional<?>){
+			displayReservation(arg);
+		}
+		
+		//customer
+		if(arg instanceof Customer){
+			displayCustomer(arg);
+		}
+		
+	}
+	
+	private void displayReservation(Object arg){
+		
+		Optional<Reservation> reserv = (Optional<Reservation>)arg;
+		if(reserv.isPresent()){
+			Reservation dawsonReserv = reserv.get();
+			Customer cust = dawsonReserv.getCustomer();
+			displayCustomer(cust);
+		}	
+	}
+	
+	private void displayCustomer(Object arg){
+		System.out.println("\nCustomer information");
+		Customer cust = (Customer)arg;
+		Name name = cust.getName();
+		String fullname = name.getFullName();
+		Email email = cust.getEmail();
+		String emailstr = email.toString();
+		Optional<CreditCard> card = cust.getCreditCard();
+		AbstractCreditCard creditCard = (AbstractCreditCard) card.get();			
+		
+		System.out.println("Name: " + fullname);
+		System.out.println("Email: " + emailstr);
+		
+		if(card.isPresent()){
+			System.out.println("Credit Card: " + creditCard.toString());
+		}
+		else{
+			System.out.println("\nNo credit card on file.");
+		}
+	}
+	
+	
+	
 
 }
